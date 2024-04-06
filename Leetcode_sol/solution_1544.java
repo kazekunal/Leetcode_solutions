a7d8713aea0ac105f38a3db07bdc8bd0f9d933a2
kunal passan <Kunalpassan30@gmail.com>
2024-04-06 22:13:53 +0530

    solution-1544: string great

---
+++ Leetcode_sol/solution_1544.java
@@ -0,0 +1,19 @@
+//make the string great!!
+
+package Leetcode_sol;
+
+class solution_1544{
+    public static String makeGood(String s) {
+        for(int i =0; i < s.length() - 1; i++){
+            //s.charAt(i) == s.charAt(i+1) + 32 || s.charAt(i) + 32 == s.charAt(i+1)
+            if(Math.abs(s.charAt(i) - s.charAt(i+1)) == 32){
+                return makeGood(s.substring(0,i) + s.substring(i+2));
+            }
+        }
+        return s;
+    }
+    public static void main(String[] args) {
+        String s = "abBAcC";
+        System.out.println(makeGood(s));
+    }
+}
