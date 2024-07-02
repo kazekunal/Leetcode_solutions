6876a046bea1b4bed719252cf3823e0dc6806c24
kunal passan <Kunalpassan30@gmail.com>
2024-07-02 22:35:19 +0530

    sol-73

--- Leetcode_sol/Solution_66.java
+++ Leetcode_sol/Solution_66.java
@@ -1,3 +1,4 @@
+
 package Leetcode_sol;
 import java.util.*;
 
---
+++ Leetcode_sol/Solution_73.java
@@ -0,0 +1,46 @@
+package Leetcode_sol;
+
+public class Solution_73 {
+        public void setZeroes(int[][] matrix) {
+          final int m = matrix.length;
+          final int n = matrix[0].length;
+          boolean F_Row = false;
+          boolean F_col = false;
+      
+          for (int j = 0; j < n; ++j)
+            if (matrix[0][j] == 0) {
+              F_Row = true;
+              break;
+            }
+      
+          for (int i = 0; i < m; ++i)
+            if (matrix[i][0] == 0) {
+              F_col = true;
+              break;
+            }
+      
+          // Store the information in the first row and the first column.
+          for (int i = 1; i < m; ++i)
+            for (int j = 1; j < n; ++j)
+              if (matrix[i][j] == 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+              }
+      
+          // Fill 0s for the matrix except the first row and the first column.
+          for (int i = 1; i < m; ++i)
+            for (int j = 1; j < n; ++j)
+              if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                matrix[i][j] = 0;
+      
+          // Fill 0s for the first row if needed.
+          if (F_Row)
+            for (int j = 0; j < n; ++j)
+              matrix[0][j] = 0;
+      
+          // Fill 0s for the first column if needed.
+          if (F_col)
+            for (int i = 0; i < m; ++i)
+              matrix[i][0] = 0;
+        }
+      }
