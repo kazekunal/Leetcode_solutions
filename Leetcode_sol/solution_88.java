ff60b054b24efbd773237cea7a44a81d3ccc3483
kunal passan <Kunalpassan30@gmail.com>
2024-07-22 14:31:10 +0530

    sol-88

---
+++ Leetcode_sol/solution_88.java
@@ -0,0 +1,32 @@
+package Leetcode_sol;
+
+public class solution_88 {
+    public static void merge(int[] nums1,int[] nums2, int m, int n){
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n -1;
+
+        while(j>=0){
+            if(i>=0 && nums1[i]>nums2[j]){
+                nums1[k] = nums1[i];
+                k--;
+                i--;
+            }
+            else{
+                nums1[k] = nums2[j];
+                k--;
+                j--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,3,5};
+        int n = nums2.length;
+        int m = nums1.length - n;
+        merge(nums1, nums2, m, n);
+        for(int i=0;i<m+n;i++){
+            System.out.print(nums1[i] + " ");
+        }
+    }
+}
