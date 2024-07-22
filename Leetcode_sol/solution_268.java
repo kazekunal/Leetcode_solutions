a5b3d1bb7f75f8a2dde2d002903d5dbe7e2cb99f
kunal passan <Kunalpassan30@gmail.com>
2024-07-22 15:32:11 +0530

    sol-268

---
+++ Leetcode_sol/solution_268.java
@@ -0,0 +1,19 @@
+package Leetcode_sol;
+
+public class solution_268 {
+    public static void missingnumber(int[] arr){
+        int n = arr.length;
+        int act_sum = (n * (n + 1)) / 2;
+        int sum = 0;
+        for(int j = 0; j < n; j++){
+            sum += arr[j];
+        }
+        int diff = act_sum - sum;
+        System.out.print(diff);
+    }
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,3,5};
+        missingnumber(arr);
+    }
+    
+}
