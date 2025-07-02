dfaf8292b324ed91070f6f3d8a76290c62d630ae
kunal passan <Kunalpassan30@gmail.com>
2025-07-03 01:24:15 +0530

    leetcode 1752

---
+++ Leetcode_sol/Leetcode_array/rotate_array.java
@@ -0,0 +1,18 @@
+package Leetcode_sol.Leetcode_array;
+
+public class rotate_array {
+    public static boolean check(int[] arr){
+        int res = 0;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] > arr[(i+1) % arr.length]){
+                res++;
+            }
+            if(res > 1) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,3,2,5};
+        System.out.println(check(arr));
+    }
+}
