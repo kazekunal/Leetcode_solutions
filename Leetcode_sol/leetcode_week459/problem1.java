f1005eed73f620c5fc1b6d2e1577d8bfe633bb53
kunal passan <Kunalpassan30@gmail.com>
2025-07-28 23:02:02 +0530

    solutions

---
+++ Leetcode_sol/leetcode_week459/problem1.java
@@ -0,0 +1,15 @@
+package Leetcode_sol.leetcode_week459;
+
+public class problem1 {
+    public static boolean check(int n){
+
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        int tens = n/10;
+        int unit = n%10;
+        System.out.println(unit);
+        System.out.println(tens);
+        System.out.println(1 % 10);
+    }
+}
