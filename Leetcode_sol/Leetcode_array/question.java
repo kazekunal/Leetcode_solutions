4e04e59110374a0a150b81bab440c58f679d8f5b
kunal passan <Kunalpassan30@gmail.com>
2025-08-10 23:53:09 +0530

    hello_java

---
+++ Leetcode_sol/Leetcode_array/question.java
@@ -0,0 +1,10 @@
+package Leetcode_sol.Leetcode_array;
+
+public class question {
+    public static int hello(){
+        return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println(hello());
+    }
+}
